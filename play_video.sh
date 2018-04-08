9e55e29f07051d32790fbf3f7b22f92fbfce43a9
Bereket Abraham <babraham42@gmail.com>
2018-04-07 21:30:04 -0700

    addded phantomjs load, fixed response page formatting

--- play_video.sh
+++ play_video.sh
@@ -2,11 +2,12 @@
 
 export DISPLAY=:0
 eritv_url=$(python /home/pi/extract_url.py)
-# http://108.201.105.106:1935
 
 if [ "$eritv_url" = "NOT_FOUND" ]; then
     echo "eritv url not found"
     eritv_url="http://108.201.105.106:1935/live/myStream/playlist.m3u8"
 fi
+
+/usr/bin/phantomjs load_url.js
 
 livestreamer hlsvariant://${eritv_url} best --player "omxplayer -o hdmi --timeout 20 -b" --fifo
