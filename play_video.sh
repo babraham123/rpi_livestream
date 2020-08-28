1c4776c788b9beb60d66aabeed091e3b16318ced
Bereket Abraham <babraham42@gmail.com>
2020-08-27 18:43:11 -0700

    Update play_video.sh

--- play_video.sh
+++ play_video.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# The url may change after several months. To find the current one, load http://www.eastafro.com/EriTV1/ with the
+# developer tools sidebar open. Under the network tab, search for "playlist.m3u8" and copy the url.
+
 eritv_url="http://45.27.53.1:1935/live/myStream/playlist.m3u8"
 
 /home/babraham/heartbeat.sh
