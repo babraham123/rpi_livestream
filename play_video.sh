e2b93cad92d8afa270826ada7980d7f9758e389d
Bereket Abraham <babraham42@gmail.com>
2023-01-08 15:51:25 -0800

    Update m3u8 url

--- play_video.sh
+++ play_video.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# The url may change after several months. To find the current one, load http://www.eastafro.com/EriTV1/ with the
-# developer tools sidebar open. Under the network tab, search for "playlist.m3u8" and copy the url.
+# The url may change after several months. To find the current one, load https://www.dendenmedia.com/eri-tv/ with the
+# developer tools sidebar open. Under the network tab, search for "*.m3u8" and copy the url.
 
-eritv_url="http://45.27.53.1:1935/live/myStream/playlist.m3u8"
+eritv_url="https://jmc-live.ercdn.net/eritreatv/eritreatv.m3u8"
 
-/home/babraham/heartbeat.sh
+# /home/babraham/heartbeat.sh
 /usr/local/bin/streamlink hlsvariant://${eritv_url} best --player "/usr/bin/omxplayer --timeout 20" --fifo
