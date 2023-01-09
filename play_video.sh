c945bf3d1438e578380e7dd71ab4c6fca1c87dcb
Bereket Abraham <babraham42@gmail.com>
2023-01-08 16:11:04 -0800

    remove https

--- play_video.sh
+++ play_video.sh
@@ -3,7 +3,7 @@
 # The url may change after several months. To find the current one, load https://www.dendenmedia.com/eri-tv/ with the
 # developer tools sidebar open. Under the network tab, search for "*.m3u8" and copy the url.
 
-eritv_url="https://jmc-live.ercdn.net/eritreatv/eritreatv.m3u8"
+eritv_url="http://jmc-live.ercdn.net/eritreatv/eritreatv.m3u8"
 
 # /home/babraham/heartbeat.sh
 /usr/local/bin/streamlink hlsvariant://${eritv_url} best --player "/usr/bin/omxplayer --timeout 20" --fifo
